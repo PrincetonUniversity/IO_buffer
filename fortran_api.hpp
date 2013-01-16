1b9005bcb3f9a5220588a625b7c586f66ccb6e7a
Florian Libisch <flibisch@princeton.edu>
2013-01-16 03:13:08 +0000

    Updated AP with for_buf and pool_id

--- fortran_api.hpp
+++ fortran_api.hpp
@@ -1,28 +1,37 @@
-
+#ifndef FORTRAN_API_DEF
+#define FORTRAN_API_DEF
 
 #define FINT long long; 
 
+extern "C" 
+void for_buf_construct( const FINT& maxmem, const FINT& blocksize, const FINT& storagepolicy, const FINT& nthread, FINT& pool_id );
 
 extern "C" 
-void construct( const FINT& maxmem, const FINT& blocksize, const FINT& storagepolicy, const FINT& nthread);
-
-extern "C" 
-void openfile( const FINT& unit, const int& length, const char* filename);
+void for_buf_openfile( const FINT& pool_id, const FINT& unit, const int& length, const char* filename);
 
 extern "C"
-void writeElement( const FINT& unit, const FINT& pos, const double& value, const FINT& threadnum);
+void for_buf_writeElement( const FINT& unit, const FINT& pos, const double& value, const FINT& threadnum);
 
 extern "C"
-void readElement( const FINT& unit, const FINT& pos, double& value, const FINT& threadnum);
+void for_buf_readElement( const FINT& unit, const FINT& pos, double& value, const FINT& threadnum);
 
 // close file, flush before closing
 extern "C"
-void closefile( const FINT& unit);
+void for_buf_closefile( const FINT& unit);
 
-// delete file, no flush
+// delete file, free memory, no flush
 extern"C"
-void removefile( const FINT& unit);
+void for_buf_removefile( const FINT& unit);
 
 // write all buffers to file
 extern "C"
-void flushfile( const FINT& unit);
+void for_buf_flushfile( const FINT& unit);
+
+extern "C" 
+void for_buf_closepool( const FINT& pool_id );
+
+// flush all files
+extern "C"
+void for_buf_flushall( const FINT& pool_id );
+
+#endif
