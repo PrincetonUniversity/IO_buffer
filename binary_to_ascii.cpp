d4b76092d52db7be534ffbaab2c26b452673eb6c
Johannes Dieterich <jmd2@princeton.edu>
2013-01-24 23:26:47 +0000

    Fix this and clean error messages up on the way.

--- binary_to_ascii.cpp
+++ binary_to_ascii.cpp
@@ -7,18 +7,19 @@
 
 int main(int argc, char** argv){
   if (argc < 2){
-    std::cerr << "Argument count too small, usage:\n binary_to_ascii filename [nblock]\n";
+    std::cerr << "Argument count too small, usage:\n binary_to_ascii filename [blocksize]\n";
+    std::cerr << "Argument size is " << argc << "\n";
     return -1;
   }
   std::ifstream ifs(argv[1]);
   size_t block_size = 1;
   if (argc > 2){
-    std::istringstream iss(argv[1]);
+    std::istringstream iss(argv[2]);
     iss >> block_size;
   }
 
   if (block_size == 0){
-    std::cerr << "Argument count too small, usage:\n binary_to_ascii filename [nblock]\n";
+    std::cerr << "Block size is zero, please specify a correct block size.\n";
     return -1;
   }
 
